746fb443ff4a98c19fc661de94d47e51bc291671
Johan Mabille <jmabille@bloomberg.net>
2016-02-26 10:25:40 +0100

    numpy_array.h added to setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'include/pybind11/descr.h',
         'include/pybind11/eigen.h',
         'include/pybind11/numpy.h',
-        'include/pybind11/numpy_array.h'
+        'include/pybind11/numpy_array.h',
         'include/pybind11/pybind11.h',
         'include/pybind11/stl.h',
         'include/pybind11/common.h',
